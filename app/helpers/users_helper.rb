fcedd5447df10da613e7d321004b5ee365b6305e
avellanas <hvanaels@gmail.com>
2014-05-10 09:51:32 -0400

    Add tests for error messages, flash

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,11 @@
 module UsersHelper
 
   # Returns the Gravatar (http://gravatar.com/) for the given user.
-  def gravatar_for(user)
+  def gravatar_for(user, options = { size: 50 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
     gravatar_d = ["identicon", "monsterid", "wavatar", "retro"].shuffle[0]
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?d=#{gravatar_d}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=#{gravatar_d}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
